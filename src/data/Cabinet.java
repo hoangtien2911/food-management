f4cb8c1680634f3d1fcd0c07bbc18ccd42ddd77f
hoangtien2911 <hoangtienbmt2911@gmail.com>
2022-07-17 23:31:15 +0700

    hihi

--- src/data/Cabinet.java
+++ src/data/Cabinet.java
@@ -1,4 +1,3 @@
-
 package data;
 
 import java.io.BufferedWriter;
@@ -9,12 +8,12 @@ import util.MyToys;
 
 /**
  *
- * @author Hoang-Tien | fb/htmsyunh/
- * Version2 2-10-2021
+ * @author Hoang-Tien | fb/htmsyunh/ Version2 2-10-2021
  */
 public class Cabinet {
+
     private ArrayList<Food> foodList = new ArrayList();
-    
+
     public void addFood() {
         String id, name, type, fileName;
         int place;
@@ -22,33 +21,34 @@ public class Cabinet {
         LocalDate expiredDay;
         int pos;
         do {
-            id = MyToys.getID("Input ID(FXXXX, X stands for digit): ", 
-                "Please input ID again with correct format!!!", "[F|f]\\d{4}$");
+            id = MyToys.getID("Input ID(FXXXX, X stands for digit): ",
+                    "Please input ID again with correct format!!!", "[F|f]\\d{4}$");
             pos = searchFoodByIDForInput(id);
-            if (pos >= 0)
+            if (pos >= 0) {
                 System.out.println("The food id already exists."
                         + " Input another one!");
+            }
         } while (pos != -1);
-        
+
         name = MyToys.getString("Input name of food: ",
-                        "Please input again name of food!");
-        
+                "Please input again name of food!");
+
         weight = MyToys.getDouble("Input weight of food(0,001 - 10 kg): ",
-                        "Please input weight of food again!", 0.001, 10);
-        
+                "Please input weight of food again!", 0.001, 10);
+
         type = MyToys.getString("Input type of food: ",
-                        "Please input again type of food");
-        
-        place = MyToys.getInteger("Place (Choose number 1(Freezer); 2(Freezer Door); 3(Regular); 4(Door)): "
-                                , "Please input again by a number you choose", 1, 4);
-        
+                "Please input again type of food");
+
+        place = MyToys.getInteger("Place (Choose number 1(Freezer); 2(Freezer Door); 3(Regular); 4(Door)): ",
+                 "Please input again by a number you choose", 1, 4);
+
         expiredDay = MyToys.inputValidDate("Input expired day of food(yyyy-mm-dd): ");
-        
+
         foodList.add(new Food(id, name, weight, type, place, expiredDay));
         System.out.println("Input new food successfully!!!");
-        
+
         fileName = MyToys.getString("Input name of file(.dat) you want to save: ",
-                        "Please input name of file again");
+                "Please input name of file again");
         try {
             FileWriter fw = new FileWriter(fileName + ".dat");
             BufferedWriter bw = new BufferedWriter(fw);
@@ -61,44 +61,48 @@ public class Cabinet {
         } catch (Exception e) {
         }
     }
-    
+
     public int searchFoodByIDForInput(String id) {
-        if (foodList.isEmpty())
+        if (foodList.isEmpty()) {
             return -1;
+        }
         for (int i = 0; i < foodList.size(); i++) {
-            if (foodList.get(i).getId().equalsIgnoreCase(id))
-            return i;
+            if (foodList.get(i).getId().equalsIgnoreCase(id)) {
+                return i;
+            }
         }
         return -1;
     }
-    
+
     public Food searchFoodByID(String id) {
-        if (foodList.isEmpty())
+        if (foodList.isEmpty()) {
             return null;
+        }
         for (int i = 0; i < foodList.size(); i++) {
-            if (foodList.get(i).getId().equalsIgnoreCase(id))
+            if (foodList.get(i).getId().equalsIgnoreCase(id)) {
                 return foodList.get(i);
+            }
         }
         return null;
     }
-    
+
     public void searchFoodByID() {
         String id;
-        id = MyToys.getID("Input ID you want to search(FXXXX, X stands for digit): ", 
+        id = MyToys.getID("Input ID you want to search(FXXXX, X stands for digit): ",
                 "Please input ID again with correct format!!!", "[F|f]\\d{4}$");
         Food x = searchFoodByID(id);
-        if (x == null)
+        if (x == null) {
             System.out.println("No food found on the list");
-        else {
+        } else {
             System.out.println("Food is found. There is: ");
             String header = String.format("|%-5s|%-20s|%6s|%-10s|%5s|%-11s|",
-                                    "ID", "NAME", "WEIGHT", "TYPE", "PLACE", "EXPIRED DAY");
+                    "ID", "NAME", "WEIGHT", "TYPE", "PLACE", "EXPIRED DAY");
             System.out.println(header);
             x.showListFood();
         }
-            
+
     }
-    
+
     public void removeAFoodByID() {
         String id;
         String yes = "yes", no = "no";
@@ -111,20 +115,22 @@ public class Cabinet {
         } else {
             System.out.println("Food is found. Food in the list before remove:");
             foodList.get(x).showListFood();
+            answer = MyToys.getString("Do you want to remove food in the list?  ", "Please answer yes or no!!!");
+            if (answer.equalsIgnoreCase(yes)) {
+                foodList.remove(x);
+                System.out.println("Food has been removed from the list");
+            }
+            if (answer.equalsIgnoreCase(no)) {
+                System.out.println("Food will not be deleted!");
+            }
         }
-        answer = MyToys.getString("Do you want to remove food in the list?  ", "Please answer yes or no!!!");
-        if (answer.equalsIgnoreCase(yes)) {
-            foodList.remove(x);
-            System.out.println("Book has been removed from the list");
-        }
-        if (answer.equalsIgnoreCase(no)) {
-            System.out.println("Food will not be deleted!");
-        }
+
     }
-    
+
     public void printFoodList() {
-        if (foodList.isEmpty())
+        if (foodList.isEmpty()) {
             System.out.println("Food list is empty! Nothing to print.");
+        }
         for (int i = 0; i < foodList.size() - 1; i++) {
             for (int j = i + 1; j < foodList.size(); j++) {
                 if (foodList.get(i).getExpiredDay().isBefore(foodList.get(j).getExpiredDay())) {
@@ -135,7 +141,7 @@ public class Cabinet {
             }
         }
         String header = String.format("|%-5s|%-20s|%6s|%-10s|%5s|%-11s|",
-                                    "ID", "NAME", "WEIGHT", "TYPE", "PLACE", "EXPIRED DAY");
+                "ID", "NAME", "WEIGHT", "TYPE", "PLACE", "EXPIRED DAY");
         System.out.println(header);
         for (int i = 0; i < foodList.size(); i++) {
             foodList.get(i).showListFood();
